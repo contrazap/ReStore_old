22dc43055daa15aab820dc1d3cb83879ed1e5f53
contrazap <contrazap@gmail.com>
2023-04-16 10:43:50 +0530

    Created react project and edited

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+const products = [
+  { name: "product1", price: 100.0 },
+  { name: "product2", price: 200.0 },
+  { name: "product2", price: 200.0 },
+];
+
+function App() {
+  return (
+    <div>
+      <>
+        <h1>Re-Store</h1>
+        <ul>
+          {products.map((item, index) => (
+            <li key={index}>
+              {item.name} - {item.price}
+            </li>
+          ))}
+        </ul>
+      </>
+    </div>
+  );
+}
+
+export default App;
