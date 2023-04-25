910983031828b7f1c0482c6662128532eadfd7cf
contrazap <contrazap@gmail.com>
2023-04-25 20:32:44 +0530

    Added react router and creating links to pages

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,27 @@
-import { AppBar, Stack, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  List,
+  ListItem,
+  Stack,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 interface Props {
   darkMode: boolean;
@@ -16,10 +39,44 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
           alignItems="center"
           sx={{ width: "100%", paddingRight: "16px" }}
         >
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component={NavLink}
+            to="/"
+            sx={{ flexGrow: 1, color: "inherit", textDecoration: "none" }}
+          >
             RE-STORE
           </Typography>
           <Switch checked={darkMode} onChange={handleThemeChange} />
+          <List sx={{ display: "flex" }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={{ color: "inherit", typography: "h6" }}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={{ color: "inherit", typography: "h6" }}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
         </Stack>
       </Toolbar>
     </AppBar>
