ccf3bbc139cc59926473b46546bf1d73bfc5b861
contrazap <contrazap@gmail.com>
2023-04-25 21:22:30 +0530

    Used axios to get product details

--- client/src/features/Catalog/ProductDetails.tsx
+++ client/src/features/Catalog/ProductDetails.tsx
@@ -1,5 +1,25 @@
 import { Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/Product";
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product details</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <Typography variant="h2">Loading...</Typography>;
+
+  if (!product) return <Typography variant="h2">Product not found</Typography>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
